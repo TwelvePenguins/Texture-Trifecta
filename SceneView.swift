f52ed3cd421c0777b27f02ad6449e5e49751bc47
Du Yuhan <yuhan.du@gmail.com>
2023-04-18 20:31:59 +0800

    Added animations

--- SceneView.swift
+++ SceneView.swift
@@ -9,41 +9,44 @@ import SwiftUI
 
 struct SceneView: View {
     
-    let timer = Timer.publish(every: 10, tolerance: 0.5, on: .main, in: .common).autoconnect()
-
+    let timer = Timer.publish(every: 7, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    
     @Binding var collection: Collection
-    
-    @State var status: String = "Normal"
-    
     @State var rotation = 0
+    @State var penguinOffset: Double = 0
+    @State var sproutOpacity: Double = 0
     
     var body: some View {
         GeometryReader { geo in
             ZStack {
-                Image("\(collection.name) \(status)")
+                Image("\(collection.name)")
                     .scaledToFit()
                     .ignoresSafeArea()
                     .frame(maxWidth: geo.size.width * 1)
                     .onReceive(timer) { _ in
                         withAnimation {
-                            status = "Activated"
+                            sproutOpacity = 1
+                            penguinOffset = -250
                         }
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            sproutOpacity = 0
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
                             withAnimation {
-                                status = "Normal"
+                                penguinOffset = 0
                             }
                         }
                         print("Timer activated")
                     }
-//                                ForEach(collection.objects[0].parts, id: \.self) { part in
-//                                    Image(part.name)
-//                                        .resizable()
-//                                        .scaledToFit()
-//                                }
-//                                    .frame(maxWidth: geo.size.width * 0.2)
-//                                    .offset(x: geo.size.width * 0.2, y: geo.size.height * -0.2)
-//                                    .rotationEffect(.degrees(180), anchor: .center)
-//                                    .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z: 0))
+                //                                ForEach(collection.objects[0].parts, id: \.self) { part in
+                //                                    Image(part.name)
+                //                                        .resizable()
+                //                                        .scaledToFit()
+                //                                }
+                //                                    .frame(maxWidth: geo.size.width * 0.2)
+                //                                    .offset(x: geo.size.width * 0.2, y: geo.size.height * -0.2)
+                //                                    .rotationEffect(.degrees(180), anchor: .center)
+                //                                    .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z: 0))
                 
                 ForEach(collection.objects, id: \.self) { object in
                     Group {
@@ -62,11 +65,23 @@ struct SceneView: View {
                             }
                         }
                         .frame(maxWidth: object.name == "Penguin" ? geo.size.width * 0.2 : geo.size.width * 0.7)
-                        .offset(x: geo.size.width * getOffset(of: object.name)[0], y: geo.size.height * getOffset(of: object.name)[1])
+                        .offset(x: geo.size.width * getOffset(of: object.name)[0], y: object.name == "Penguin" ? geo.size.height * getOffset(of: object.name)[1] + penguinOffset: geo.size.width * getOffset(of: object.name)[1])
                         .rotationEffect(.degrees(Double(object.name == "Orca" ? 180 : rotation)), anchor: .center)
                         .rotation3DEffect(.degrees(Double(object.name == "Orca" ? 180 : rotation)), axis: (x: 1, y: 0, z: 0))
                     }
                 }
+                VStack(spacing: -240) {
+                    Text("POOF!")
+                        .font(.title)
+                        .fontDesign(.rounded)
+                        .rotationEffect(.degrees(30))
+                    Image("OrcaSprout")
+                        .resizable()
+                        .scaledToFit()
+                }
+                .offset(x: geo.size.width * 0, y: geo.size.height * 0)
+                .opacity(sproutOpacity)
+                .animation(.easeIn(duration: 0.2), value: sproutOpacity)
             }
         }
     }
